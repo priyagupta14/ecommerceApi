69f4051b42e53c223a981006589e3849da650dbd
Priya Gupta <pgupta000205@gmail.com>
2021-03-06 19:36:04 +0530

    migration for making items unique

--- src/migrations/20210306111908-alter-item-unique.js
+++ src/migrations/20210306111908-alter-item-unique.js
@@ -1,13 +1,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('Categories', 'category', {
+    await queryInterface.changeColumn('Items', 'item', {
       type: Sequelize.STRING,
       unique: true,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('Categories', 'category', {
+    await queryInterface.changeColumn('Items', 'item', {
       type: Sequelize.STRING,
       unique: false,
     });
